00e5b1bc9c45cef4bdb36494aa6f99b07c0b27a4
JaylynnValensa <thuyhangnguyen190298@gmail.com>
2024-04-04 09:38:52 +0300

    get posts and comments

--- routes/comments.js
+++ routes/comments.js
@@ -3,6 +3,18 @@ const { query } = require('../helpers/db.js');
 
 const commentRouter = express.Router()
 
+// Get all comments
+commentRouter.get('/posts/:postId/comments', async (req, res) => {
+  const id = req.params.postId;
+  try {
+    const result = await query('SELECT * FROM comments WHERE post_id =' + id );
+    const rows = result.rows ? result.rows : [];
+    res.status(200).json(rows);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+})
+
 // *WORK* Create new comment
 commentRouter.post("/posts/:post_id/comments", async(req, res) => {
     try {
